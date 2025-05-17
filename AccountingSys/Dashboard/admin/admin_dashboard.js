f493902006142ca645ef4ef29fd8fb7b3aee5e5e
System143812 <mypubacc0unt@gmail.com>
2025-05-18 01:24:38 +0800

    dami finix

--- AccountingSys/Dashboard/admin/admin_dashboard.js
+++ AccountingSys/Dashboard/admin/admin_dashboard.js
@@ -1,3 +1,11 @@
+document.getElementById('logout-icon').addEventListener('click', function () {
+  window.location.href = "../../Login/index.html";
+});
+
 document.getElementById('employee-tab').addEventListener('click', function () {
   window.location.href = "admin_employee.html";
+});
+
+document.getElementById('department-tab').addEventListener('click', function () {
+  window.location.href = "admin_department.html";
 });
--- AccountingSys/Dashboard/admin/admin_employee.js
+++ AccountingSys/Dashboard/admin/admin_employee.js
@@ -1,3 +1,11 @@
+document.getElementById('logout-icon').addEventListener('click', function () {
+  window.location.href = "../../Login/index.html";
+});
+
 document.getElementById('dashboard-tab').addEventListener('click', function () {
   window.location.href = "admin_dashboard.html";
+});
+
+document.getElementById('department-tab').addEventListener('click', function () {
+  window.location.href = "admin_department.html";
 });
