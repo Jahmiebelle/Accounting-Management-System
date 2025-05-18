de2b4a2d8f2e85d81cc2494a4b59205da7fccca3
System143812 <mypubacc0unt@gmail.com>
2025-05-18 14:06:23 +0800

    update

--- AccountingSys/Dashboard/admin/admin_dashboard.js
+++ AccountingSys/Dashboard/admin/admin_dashboard.js
@@ -1,11 +1,11 @@
 document.getElementById('logout-icon').addEventListener('click', function () {
-  window.location.href = "../../Login/index.html";
+  window.location.href = "../../Login/index.php";
 });
 
 document.getElementById('employee-tab').addEventListener('click', function () {
-  window.location.href = "admin_employee.html";
+  window.location.href = "admin_employee.php";
 });
 
 document.getElementById('department-tab').addEventListener('click', function () {
-  window.location.href = "admin_department.html";
+  window.location.href = "admin_department.php";
 });
--- AccountingSys/Dashboard/admin/admin_employee.js
+++ AccountingSys/Dashboard/admin/admin_employee.js
@@ -1,11 +1,11 @@
 document.getElementById('logout-icon').addEventListener('click', function () {
-  window.location.href = "../../Login/index.html";
+  window.location.href = "../../Login/index.php";
 });
 
 document.getElementById('dashboard-tab').addEventListener('click', function () {
-  window.location.href = "admin_dashboard.html";
+  window.location.href = "admin_dashboard.php";
 });
 
 document.getElementById('department-tab').addEventListener('click', function () {
-  window.location.href = "admin_department.html";
+  window.location.href = "admin_department.php";
 });
