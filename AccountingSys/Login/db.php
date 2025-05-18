735e0320e9224ac0b4c8bd02243b0772e6a3a223
System143812 <mypubacc0unt@gmail.com>
2025-05-18 12:57:11 +0800

    update db.php

--- AccountingSys/Login/db.php
+++ AccountingSys/Login/db.php
@@ -8,7 +8,7 @@ $port = 3306;
 $termux = "/data/data/com.termux/files/usr/var/run/mysqld.sock";
 
 // remove mo yung port & termux sa parameter if di ka sa termux
-$conn = new mysqli($servername, $username, $password, $dbname, $port, $termux); 
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port, $termux); 
 
 
 if ($conn->connect_error) {
@@ -16,6 +16,7 @@ if ($conn->connect_error) {
 }
 
 else {
-    echo "Connected to the database!";
+    echo "<script>alert('Connected to db');</script>";
+    
 }
 ?>
