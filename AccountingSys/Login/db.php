f467720ddca8ad531d2b80a8a981028daa1c4b92
System143812 <mypubacc0unt@gmail.com>
2025-05-18 13:22:09 +0800

    update

--- AccountingSys/Login/db.php
+++ AccountingSys/Login/db.php
@@ -9,14 +9,9 @@ $termux = "/data/data/com.termux/files/usr/var/run/mysqld.sock";
 // remove mo yung port & termux sa parameter if di ka sa termux
 $conn = mysqli_connect($servername, $username, $password, $dbname, 3306, $termux); 
 
-
 if (!$conn) {
-    
-    die("<script>alert('❌ DB Connection Failed: " . mysqli_connect_error() . "');</script>");
-
-    $db_status = "fail";
-}
-else {
-    $db_status = "success";
+    die("<script>alert('❌ Connection Failed: " . mysqli_connect_error() . "');</script>");
+} else {
+    echo "<script>alert('✅ Connected to DB');</script>";
 }
 ?>
