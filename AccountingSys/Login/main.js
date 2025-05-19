937e0da04077f62e98a0dd7d9546559696d50332
System143812 <mypubacc0unt@gmail.com>
2025-05-19 11:04:30 +0800

    update main.js

--- AccountingSys/Login/main.js
+++ AccountingSys/Login/main.js
@@ -6,18 +6,3 @@ function showForgot(){
   document.querySelector('.forgotPass-form').classList.toggle('show');
   document.querySelector('.login-form').classList.toggle('hide');
 }
-
-
-
-document.getElementById('login-btn').addEventListener('click', function(){
-  const company_Id = document.getElementById('lgn-company-id').value;
-  const password = document.getElementById('lgn-password').value;
-  const role = document.getElementById('position').value;
-  if(parseInt(company_Id) === 1234 && password === 'gabby'){
-    window.location.href = "../Dashboard/admin/admin_dashboard.php";
-  }
-  else{
-    alert("Invalid Credentials!! "+ role);
-  }
-  
-});
