5a1376c41c0ee9e91dfb1cbbccd9bda5793b6aa8
System143812 <mypubacc0unt@gmail.com>
2025-05-20 20:46:34 +0800

    update

--- AccountingSys/Dashboard/admin/admin_employee.php
+++ AccountingSys/Dashboard/admin/admin_employee.php
@@ -180,7 +180,7 @@ session_start();
                       <td> 
                         <form class='profile-form' action='admin_employee.php' method='POST' accept-charset='utf-8'>
                           <input type='hidden' name='emp_id' id='emp_id' value='$emp_id'/>
-                          <button class='profile-btn' type='submit'>Profile</button>
+                            <button class='profile-btn' type='submit'>Profile</button>
                         </form>
                       </td>
                     </tr>";
