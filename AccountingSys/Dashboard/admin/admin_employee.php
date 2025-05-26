73145f7b2e299e48df9da0a89e86f4a68c3ea346
System143812 <mypubacc0unt@gmail.com>
2025-05-26 18:32:11 +0800

    update admin_employee.php

--- AccountingSys/Dashboard/admin/admin_employee.php
+++ AccountingSys/Dashboard/admin/admin_employee.php
@@ -325,7 +325,7 @@ session_start();
                 </div>
                 <div class="deacbox-container">
                   <label id="deacbox-label" for="deac-emp">Deactivated Accounts</label>
-                  <input type="checkbox" name="activation" id="deac-emp"/>
+                  <input type="checkbox" name="activation" id="deac-emp" onchange="document.getElementById('search_form').submit()"/>
                 </div>
               </form>
             </div>
