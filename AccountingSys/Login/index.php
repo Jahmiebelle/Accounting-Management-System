0f3ac7b40d6c5c8e806fcb9766da7d8147f66f50
System143812 <mypubacc0unt@gmail.com>
2025-05-28 10:23:17 +0800

    debug

--- AccountingSys/Login/index.php
+++ AccountingSys/Login/index.php
@@ -37,7 +37,7 @@ session_start();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/AccountingMSG1/AccountingSys/Styles/styles.css?v=1.0">
+  <link rel="stylesheet" href="/../../Styles/styles.css?v=1.0">
   <title>Login </title>
   </head>
   
