c4eca50d89e45ef93d96c6e1bc1b6c441c99e2f1
gab <mypubacc0unt@gmail.com>
2025-06-02 15:02:21 +0800

    Update db.php

--- AccountingSys/Login/db.php
+++ AccountingSys/Login/db.php
@@ -4,10 +4,10 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "accountingsys";
-
+$termux = "/data/data/com.termux/files/usr/var/run/mysqld.sock";
 
 // remove mo yung port & termux sa parameter if di ka sa termux
-$conn = mysqli_connect($servername, $username, $password, $dbname); 
+$conn = mysqli_connect($servername, $username, $password, $dbname, 3306, $termux); 
 
 if(!$conn) {
     $db_status = "fail";
