c9ed0cf3b3fdffd9a0a8520a6105de317873b007
System143812 <mypubacc0unt@gmail.com>
2025-06-03 14:59:55 +0800

    update

--- AccountingSys/Dashboard/admin/admin_department.js
+++ AccountingSys/Dashboard/admin/admin_department.js
@@ -1,51 +1,55 @@
-document.getElementById('logout-icon').addEventListener('click', function () {
-  window.location.href = "../../Login/logout.php";
-});
+document.addEventListener('DOMContentLoaded', function(){
 
-document.getElementById('dashboard-tab').addEventListener('click', function () {
-  window.location.href = "admin_dashboard.php";
-});
-
-document.getElementById('employee-tab').addEventListener('click', function () {
-  window.location.href = "admin_employee.php";
-});
-
-document.getElementById('leavereq-tab').addEventListener('click', function () {
-  window.location.href = "admin_leavereq.php";
-});
-
-document.getElementById('payroll-tab').addEventListener('click', function () {
-  window.location.href = "admin_payroll.php";
-});
-
-document.getElementById('attendance-tab').addEventListener('click', function () {
-  window.location.href = "admin_attendance.php";
-});
-
-
-const deleteBtn = document.querySelectorAll('delete-dept');
-deleteBtn.forEach(function(delBtn){
-  delBtn.addEventListener('click', function(){
-    const deleteId = delBtn.dataset.id;
-    const deleteName = delBtn.dataset.name;
-    const confirmDel = confirm("Are you sure to delete " + deleteName + "?");
-    if(confirmDel){
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "delete_dept.php", true);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.onload = function(){
-        if (xhr.status === 200) {
-          alert(xhr.responseText);
-          setTimeout(function(){
-            location.reload();
-          }, 100);
-        }
-        else {
-          alert(xhr.responseText);
-        }
-      };
-      const data = "id=" + encodeURIComponent(deleteId);
-      xhr.send(data);
-    }
+  document.getElementById('logout-icon').addEventListener('click', function () {
+    window.location.href = "../../Login/logout.php";
   });
+  
+  document.getElementById('dashboard-tab').addEventListener('click', function () {
+    window.location.href = "admin_dashboard.php";
+  });
+  
+  document.getElementById('employee-tab').addEventListener('click', function () {
+    window.location.href = "admin_employee.php";
+  });
+  
+  document.getElementById('leavereq-tab').addEventListener('click', function () {
+    window.location.href = "admin_leavereq.php";
+  });
+  
+  document.getElementById('payroll-tab').addEventListener('click', function () {
+    window.location.href = "admin_payroll.php";
+  });
+  
+  document.getElementById('attendance-tab').addEventListener('click', function () {
+    window.location.href = "admin_attendance.php";
+  });
+  
+  
+  const deleteBtn = document.querySelectorAll('delete-dept');
+  deleteBtn.forEach(function(delBtn){
+    delBtn.addEventListener('click', function(){
+      const deleteId = delBtn.dataset.id;
+      const deleteName = delBtn.dataset.name;
+      const confirmDel = confirm("Are you sure to delete " + deleteName + "?");
+      if(confirmDel){
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "delete_dept.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onload = function(){
+          if (xhr.status === 200) {
+            alert(xhr.responseText);
+            setTimeout(function(){
+              location.reload();
+            }, 100);
+          }
+          else {
+            alert(xhr.responseText);
+          }
+        };
+        const data = "id=" + encodeURIComponent(deleteId);
+        xhr.send(data);
+      }
+    });
+  });
+
 });
