d3a41aebcbd45c6183430fd29e7b38d218dd15a3
System143812 <mypubacc0unt@gmail.com>
2025-06-04 18:56:37 +0800

    update

--- AccountingSys/Dashboard/admin/admin_dashboard.php
+++ AccountingSys/Dashboard/admin/admin_dashboard.php
@@ -119,7 +119,7 @@ session_start();
                 ?> 
               </div>
               <div class="emptybox">
-                
+                empty
               </div>
             </div>
             
