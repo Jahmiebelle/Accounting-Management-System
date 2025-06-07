d1bc5f51ce78d33d4aaad211c975783c85dc8af1
System143812 <mypubacc0unt@gmail.com>
2025-06-07 16:09:01 +0800

    update

--- AccountingSys/Dashboard/admin/update_hourly_rate.php
+++ AccountingSys/Dashboard/admin/update_hourly_rate.php
@@ -4,8 +4,23 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
   
+  $professorRate = $_POST['professor_rate'];
+  $instructorRate = $_POST['instructor_rate'];
+  $partTimeRate = $_POST['part_time_rate'];
   
+  $instructorRateQuery = "UPDATE role_hourly_rate SET hourly_rate = '$instructorRate' WHERE role_name = 'instructor';";
+  $professorRateQuery = "UPDATE role_hourly_rate SET hourly_rate = '$professorRate' WHERE role_name = 'professor';";
+  $partTimeRateQuery = "UPDATE role_hourly_rate SET hourly_rate = '$partTimeRate' WHERE role_name = 'part_time_staff';";
   
+  $instructorResult = mysqli_connect($conn, $instructorRateQuery);
+  $professorResult = mysqli_connect($conn, $professorRateQuery);
+  $partTimeResult = mysqli_connect($conn, $partTimeRateQuery);
   
-  
+  if($instructorResult && $professorResult && $partTimeResult){
+    echo "tax updated successfully";
+  }
+  else{
+    echo "can't update the tax";
+  }
+  header("Location: admin_payroll.php");
 ?>
