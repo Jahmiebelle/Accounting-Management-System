8d6c9d8e7b9ca089274b0eead4609622f3f1dcf5
System143812 <mypubacc0unt@gmail.com>
2025-06-10 20:48:25 +0800

    update clocking.php

--- AccountingSys/Dashboard/employee/clocking.php
+++ AccountingSys/Dashboard/employee/clocking.php
@@ -1,50 +1,58 @@
 <?php
-  include '../../Login/db.php';
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-  
-  $clockInId = $_POST['clock_in_id'];
-  $clockInTime = $_POST['clock_in_curtime'];
-  
-  $clockInTimeObj = new DateTime($clockInTime);
-  $clockInHour = $clockInTimeObj->format('H');
-  $clockInMin = $clockInTimeObj->format('i');
-  $clockInSecs = $clockInTimeObj->format('s');
-  $updateClockIn = "UPDATE admin_employee_attendance SET clock_in = '$clockInTime' WHERE employee_id = '$clockInId' AND employee_date = CURDATE();";
-  $clockInResult = mysqli_query($conn, $updateClockIn);
-  if($clockInResult){
-    echo "You are now Clocked in!";
-  }
-  else{
-    echo mysqli_error($conn);
-  }
-  
-  $clockOutId = $_POST['clock_out_id'];
-  $clockOutTime = $_POST['clock_out_curtime'];
-  $updateClockOut = "UPDATE admin_employee_attendance SET clock_out = '$clockOutTime' WHERE employee_id = '$clockOutId' AND employee_date = CURDATE();";
-  $clockOutResult = mysqli_query($conn, $updateClockOut);
-  if($clockOutResult){
-    echo "Clocked out! Thanks for your hardwork!";
-  }
-  else{
-    echo mysqli_error($conn);
-  }
-  $subtractInString = '-' . $clockInHour . " hours -" . $clockInMin . " minutes -" . $clockInSecs . " seconds";
-  
-  $endTime = new DateTime($clockOutTime);
-  $endTime->modify($subtractInString);
-  $todayWorkHours = $endTime->format('H:i:s');
-  $todayOvertime = "00:00:00";
-  if($endTime->format('H') > 8){
-    $todayOvertime = $endTime->modify('-8 hours');
-  }
-  $updateTotalHours = "UPDATE admin_employee_attendance SET employee_overtime = '$todayOvertime', total_hours = '$todayWorkHours';";
-  $updateHoursResult = mysqli_query($conn, $updateTotalHours);
-  if($updateHoursResult){
+include '../../Login/db.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$clockInId = $_POST['clock_in_id'];
+$clockOutId = $_POST['clock_out_id'];
+$clockInTime = $_POST['clock_in_curtime'];
+$clockOutTime = $_POST['clock_out_curtime'];
+
+$clockInTimeObj = new DateTime($clockInTime);
+$clockOutTimeObj = new DateTime($clockOutTime);
+
+$clockInFormatted = $clockInTimeObj->format('H:i:s');
+$clockOutFormatted = $clockOutTimeObj->format('H:i:s');
+
+$updateClockIn = "UPDATE admin_employee_attendance 
+    SET clock_in = '$clockInFormatted' 
+    WHERE employee_id = '$clockInId' AND employee_date = CURDATE();";
+if (mysqli_query($conn, $updateClockIn)) {
+    echo "You are now Clocked in!<br>";
+} else {
+    echo "Clock-in error: " . mysqli_error($conn) . "<br>";
+}
+
+$updateClockOut = "UPDATE admin_employee_attendance 
+    SET clock_out = '$clockOutFormatted' 
+    WHERE employee_id = '$clockOutId' AND employee_date = CURDATE();";
+if (mysqli_query($conn, $updateClockOut)) {
+    echo "Clocked out! Thanks for your hard work!<br>";
+} else {
+    echo "Clock-out error: " . mysqli_error($conn) . "<br>";
+}
+
+$interval = $clockInTimeObj->diff($clockOutTimeObj);
+$todayWorkHours = $interval->format('%H:%I:%S');
+
+$workedHours = (int)$interval->format('%H');
+$workedMins = (int)$interval->format('%I');
+$workedSecs = (int)$interval->format('%S');
+
+if ($workedHours > 8) {
+    $overtimeHours = $workedHours - 8;
+    $todayOvertime = sprintf('%02d:%02d:%02d', $overtimeHours, $workedMins, $workedSecs);
+} else {
+    $todayOvertime = '00:00:00';
+}
+
+$updateTotalHours = "UPDATE admin_employee_attendance 
+    SET employee_overtime = '$todayOvertime', total_hours = '$todayWorkHours' 
+    WHERE employee_id = '$clockOutId' AND employee_date = CURDATE();";
+if (mysqli_query($conn, $updateTotalHours)) {
     echo "Updated Total & Overtime Hours.";
-  }
-  else{
-    echo mysqli_error($conn);
-  }
+} else {
+    echo "Hours update error: " . mysqli_error($conn);
+}
 ?>
