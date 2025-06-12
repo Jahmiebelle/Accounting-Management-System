4e5d5ac59cc787f71f69f3cdd0369503effeaf8e
System143812 <mypubacc0unt@gmail.com>
2025-06-12 09:06:35 +0800

    update

--- AccountingSys/Dashboard/admin/admin_payroll.js
+++ AccountingSys/Dashboard/admin/admin_payroll.js
@@ -18,6 +18,15 @@ document.getElementById('attendance-tab').addEventListener('click', function () 
   window.location.href = "admin_attendance.php";
 });
 
+const tableStatus = document.getElementById('table-status');
+if(tableStatus.innerText = "pending"){
+  tableStatus.classList.add('pending');
+}
+else{
+  tableStatus.classList.remove('pending');
+}
+
+
 const editTaxBtn = document.getElementById('edit-tax-btn');
 const editRateBtn = document.getElementById('edit-rate-btn');
 const incomeTaxInput = document.getElementById('income-tax-input');
@@ -151,3 +160,4 @@ document.getElementById('download-payslip-btn').addEventListener('click', functi
     button.style.display = 'inline-block';
   });
 });
+
