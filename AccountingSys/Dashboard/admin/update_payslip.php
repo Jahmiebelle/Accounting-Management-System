2610a097e88c042aca5976348571e2d42e46a0a1
System143812 <mypubacc0unt@gmail.com>
2025-06-15 07:43:03 +0800

    update update_payslip.php

--- AccountingSys/Dashboard/admin/update_payslip.php
+++ AccountingSys/Dashboard/admin/update_payslip.php
@@ -65,7 +65,41 @@
       $completePreviousResult = mysqli_query($conn, $completePreviousQuery);
       $resetWorkTableQuery = "UPDATE employee_work_table SET total_hours_worked = 0, total_overtime_hours = 0, total_working_days = 0 WHERE employee_id = $employee_id;";
       $resetWorkTableResult = mysqli_query($conn, $resetWorkTableQuery);
-      //after i complete a mag reset.. I rerecompute na yung payslips this month
+      //icheck yung current value ng attendance for this month then rerecompute work table, kahit walang attendance, for accidental purposes lng
+      $getTotalHoursQuery = "SELECT employee_id, SEC_TO_TIME(SUM(TIME_TO_SEC(total_hours))) AS total_hours FROM admin_employee_attendance WHERE employee_id = $employee_id AND DATE_FORMAT(employee_date, '%Y-%m') = '$currentMonth';";
+      $getTotalOvertimeQuery = "SELECT employee_id, SEC_TO_TIME(SUM(TIME_TO_SEC(employee_overtime))) AS total_overtime FROM admin_employee_attendance WHERE employee_id = $employee_id AND DATE_FORMAT(employee_date, '%Y-%m') = '$currentMonth';";
+      $countDaysQuery = "SELECT COUNT(attendance_id) AS days_of_work FROM admin_employee_attendance WHERE employee_id = $employee_id AND DATE_FORMAT(employee_date, '%Y-%m') = '$currentMonth';";
+      
+      $totalHoursResult = mysqli_query($conn, $getTotalHoursQuery);
+      $totalOvertimeResult = mysqli_query($conn, $getTotalOvertimeQuery);
+      $countDaysResult = mysqli_query($conn, $countDaysQuery);
+      
+      $totalHoursRow = mysqli_fetch_assoc($totalHoursResult);
+      $totalOvertimeRow = mysqli_fetch_assoc($totalOvertimeResult);
+      $totalDaysRow = mysqli_fetch_assoc($countDaysResult);
+      
+      $totalHoursTime = $totalHoursRow['total_hours'];
+      $totalOvertimeTime = $totalOvertimeRow['total_overtime'];
+      $totalDayCount = (int)$totalDaysRow['days_of_work'];
+      
+      //seperate yung time into 3parts like this 12:01:00 into ['12', '01', '00'] then acces yung index zero para makuha yung hours, then yun yung ilalagay sa employee_work_table hehe
+      $totalHoursArray = explode(':', $totalHoursTime);
+      $totalHours = (int)$totalHoursArray[0];
+      $totalOvertimeArray = explode(':', $totalOvertimeTime);
+      $totalOvertime = (int)$totalOvertimeArray[0];
+      
+      //now update na natin employee_work_table
+      $updateWorkHoursQuery = "UPDATE employee_work_table SET total_hours_worked = $totalHours, total_overtime_hours = $totalOvertime, total_working_days = $totalDayCount WHERE employee_id = $clockOutId;";
+      $workHoursResult = mysqli_query($conn, $updateWorkHoursQuery);
+      if($workHoursResult){
+        echo $overtimeHoursWorked;
+      }
+      else{
+        echo mysqli_error($conn);
+      }
+          
+      
+      //after i complete at mag reset.. I rerecompute na yung payslips this month
       $empWorkQuery = "SELECT * FROM employee_work_table WHERE employee_id = '$employee_id';";
       $empWorkResult = mysqli_query($conn, $empWorkQuery);
       $empWorkRow = mysqli_fetch_assoc($empWorkResult);
