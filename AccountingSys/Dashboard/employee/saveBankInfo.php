588eb717ef3234719dca64606502e3fcdf6a4af6
System143812 <mypubacc0unt@gmail.com>
2025-06-16 19:38:26 +0800

    bank save backend

--- AccountingSys/Dashboard/employee/saveBankInfo.php
+++ AccountingSys/Dashboard/employee/saveBankInfo.php
@@ -5,6 +5,14 @@
   error_reporting(E_ALL);
   session_start();
   
+  $empId = $_SESSION['employee_id'];
+  $bank = $_POST['bank'];
+  $sss = $_POST['sss'];
+  $philhealth = $_POST['philhealth'];
+  $pagibig = $_POST['pagibig'];
+  
+  $updateEmployeeBank = "UPDATE employee_table SET bank_number = '$bank', sss_number = '$sss', philhealth_number = '$philhealth', pagibig_number = '$pagibig' WHERE employee_id = '$empId';";
+  $employeeBankResult =  mysqli_query($conn, $updateEmployeeBank);
   
   header("Location: employee_profile.php");
   
